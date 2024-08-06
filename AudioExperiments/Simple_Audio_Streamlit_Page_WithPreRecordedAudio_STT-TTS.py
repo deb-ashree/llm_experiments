a39ebe056661854e978c751ac51ca3f9afdc6922
deb-ashree <debashree.office@gmail.com>
2024-08-06 10:53:16 +0530

    file with changes

--- AudioExperiments/Simple_Audio_Streamlit_Page_WithPreRecordedAudio_STT-TTS.py
+++ AudioExperiments/Simple_Audio_Streamlit_Page_WithPreRecordedAudio_STT-TTS.py
@@ -7,33 +7,25 @@ import warnings
 from dotenv import load_dotenv
 
 load_dotenv(os.getcwd()+"/local.env")
-
+parent_folder = "AudioExperiments/"
 # Ignore DeprecationWarning
 #warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-st.title("A Talking Bot")
+st.title("Audio Bot Experiments")
 
-os.makedirs("text",  exist_ok=True)
-os.makedirs("audio",  exist_ok=True)
+os.makedirs(parent_folder+"text",  exist_ok=True)
+os.makedirs(parent_folder+"audio",  exist_ok=True)
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 client = OpenAI()
 
-audio_file = st.file_uploader("Upload Audio : ", type=["wav","mp3","m4a"])
+afile = st.file_uploader("Upload Audio : ", type=["wav","mp3","m4a"])
 st.markdown(''' *:grey[You can find some samples to download here] https://pixabay.com/music/search/vocal%20samples/*''')
-print(audio_file)
-print(type(audio_file))
+print(afile)
+print(type(afile))
+st.audio(afile)
 
-# upload_audio = st.button("Wish to upload")
-# if upload_audio:
-#     audio_file = st.file_uploader("Upload Audio : ", type=["wav","mp3","m4a"])
-#     print(audio_file)
-# else:
-#     #Default
-#     audio_file = Path(__file__).parent / "original_audio/rise-and-shine-203779.mp3"
-#     print(audio_file)
-
-text_file_path = os.path.join("text/", "audio-text.txt")
-created_audio_file_path = os.path.join("audio/", "text-audio.mp3")
+text_file_path = os.path.join(parent_folder+"text/", "audio-text.txt")
+created_audio_file_path = os.path.join(parent_folder+"audio/", "text-audio.mp3")
 
 def read_text():
     with open(text_file_path, 'r') as file:
@@ -42,7 +34,8 @@ def read_text():
 def transcribe_audio():
     transcription = client.audio.transcriptions.create(
     model="whisper-1", 
-    file=audio_file)
+    file=afile)
+    print(os.getcwd())
     return transcription.text
 
 def back_to_audio():
@@ -55,9 +48,11 @@ def back_to_audio():
 
 if st.sidebar.button("Transcribe audio to text"):
     content = transcribe_audio()
+    print("Content :\n"+content)
     st.text_area("Audio Text ", content, height=500)
     with open(text_file_path, 'w') as file:
         file.writelines(content) 
+        print(content)
     st.markdown("Convert audio to text done")
 
 if st.sidebar.button("Convert text to audio"):
@@ -67,8 +62,8 @@ if st.sidebar.button("Convert text to audio"):
     print(created_audio_file_path)
     st.audio(created_audio_file_path)
     st.markdown("**Play original audio**")
-    print(audio_file)
-    st.audio(audio_file)
+    print(afile)
+    st.audio(afile)
     st.markdown("**The converted text of the audio**")
     st.markdown(read_text())
 
